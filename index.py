06513fb38739de4b3a906fff7ab13ff463250067
okezieuc <53785400+okezieuc@users.noreply.github.com>
2023-05-13 11:22:35 +0100

    write code for the cycle and for loading ROM

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ class cpu(pyglet.window.Window):
         self.load_rom(sys.argv[1])
         while not self.has_exit:
             self.dispatch_events()
-            self.cycle
+            self.cycle()
             self.draw()
 
     def initialize(self):
@@ -31,3 +31,34 @@ class cpu(pyglet.window.Window):
             # load 80-char font set
             self.memory[i] = self.fonts[i]
             i += 1
+    
+    def load_rom(self, rom_path):
+        # log("Loading %s..." % rom_path)
+        binary = open(rom_path, "rb").read()
+        i = 0
+        while i < len(binary):
+            self.memory[i+0x200] = ord(binary[i])
+
+    def cycle(self):
+        self.opcode = self.memory[self.pc]
+
+        self.vx = (self.opcode & 0x0f00) >> 8
+        self.vy = (self.opcode & 0x00f0) >> 4
+        
+        self.pc += 2
+
+        extracted_op = self.opcode & 0xf000
+        try:
+            self.funcmap[extracted_op]()
+        except:
+            print("Unknown instruction: %X" % self.opcode)
+
+        # decrement timers
+        if self.delay_timer > 0:
+            self.delay_timer -= 1
+        if self.sound_timer > 0:
+            self.sound_timer -= 1
+            if self.sound_timer == 0:
+                # Play a sound here with pyglet!
+
+
